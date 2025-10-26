40028e0fbbb8d776b4393fa2264c1fc17a4520d1
ec1025858 <ec1025858@otc.edu>
2025-10-26 15:21:06 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Creating interger i to use with the for loop
+
+for (int i = 1; i <= 12; i++)
+{
+    // Creating interger j
+    int j = i;
+// Making the table
+    int result = i * j;
+    Console.WriteLine($" | {i} |");
+    
+     Console.WriteLine("=========================");
+    Console.Write($"{j} | {result} |");
+    Console.WriteLine("=========================");
+};
