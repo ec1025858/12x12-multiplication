db7b98578dd6597dd74d108e84195f17ea9837d7
ec1025858 <ec1025858@otc.edu>
2025-10-30 23:55:52 -0500

    Add files via upload

---
+++ 12x12 multiplication table/Program.cs
@@ -0,0 +1,16 @@
+﻿// Creating interger i to use with the for loop
+
+for (int i = 1; i <= 12; i++)
+{
+    // Creating interger j
+    int j = i;
+// Making the table
+    int result = i * j;
+    Console.Write($"  | {i} |");
+     Console.WriteLine("");
+     Console.WriteLine("=========================");
+    Console.Write($"{i} | {result} |");
+     Console.WriteLine("");
+    Console.WriteLine("=========================");
+
+};
